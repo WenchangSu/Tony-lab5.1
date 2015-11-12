2576c3fc7831711d49337ce99fbe33cebc345df8
saasbook <saasbook@saasbook.(none)>
2015-11-12 01:19:47 -0400

    1.As a blog owner, I can write new posted blogs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogLab51::Application.config.secret_token = '449c1e68a9ba3ccf38da08861ff45b17fbed2fd4ca8b2ec3b690644dbd17e4f2e8bab09b758e2610a3269b0bb0c82b2cdba1dd53c3d8965c9e468252fe402402'
